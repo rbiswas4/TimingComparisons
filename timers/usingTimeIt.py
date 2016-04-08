5916c0078164ed59a71775f33f7047f985195a97
R. Biswas <rbiswas4@gmail.com>
2016-04-07 23:43:31 -0700

    Adding usingTimeit to show use of timeing with np.sum and for loops compared 	modified:   ../SimpleArrayOperationTimings_Pandas.ipynb 	new file:   usingTimeIt.py

---
+++ timers/usingTimeIt.py
@@ -0,0 +1,45 @@
+import timeit
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+sns.set()
+
+
+
+# using setitem
+t = timeit.Timer("print 'main statement'", "print 'setup'")
+
+print 'TIMEIT:'
+print t.timeit(2)
+
+print 'REPEAT:'
+print t.repeat(3, 2)
+
+print "DONE"
+
+def sumvars(x):
+    res = 0.
+    for e in x:
+        res += e
+    return res
+res = [] 
+yres = [] 
+for n in np.logspace(1, 6, 15):
+    num = str(n)
+    setup = "import numpy as np; n = " + num + " ; x=np.random.random(n)"
+    ysetup = setup + "; from __main__ import sumvars "
+    t = timeit.Timer("np.sum(x)", setup)
+    y = timeit.Timer("sumvars(x)", ysetup)
+    # t = timeit.Timer("np.sum(x)", "import numpy as np; n = 50000000; x=np.random.random(n)")
+    vals = t.repeat (3, 2000)
+    yvals = y.repeat (3, 2000)
+    res.append([n] + list(vals))
+    yres.append([n] + list(yvals))
+    print (n)
+print(res)
+res = np.asarray(res)
+yres = np.asarray(yres)
+plt.errorbar(res[:, 0], res[:, 1:].mean(axis=1) , yerr=res[:, 1:].std(axis=1), fmt='o')
+plt.errorbar(yres[:, 0], yres[:, 1:].mean(axis=1) , yerr=yres[:, 1:].std(axis=1), fmt='s')
+plt.savefig('fig.png')
+plt.show()
